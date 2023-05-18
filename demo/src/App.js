694f2ef3f461a8b37556f294425ea44030268222
jyothirmayiLakumarapu <jyothirmayilakumarapu06@gmail.com>
2023-05-18 12:56:02 +0530

    Pushing files

---
+++ demo/src/App.js
@@ -0,0 +1,111 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react'
+import image1 from './assets/image1.jpg';
+import image2 from './assets/image2.jpg';
+import image3 from './assets/image3.jpg';
+
+
+const htmlCont = () => {
+  return (
+    <div id="content1" className="content">
+      <div className="content-left">
+        <h1>Learn HTML</h1>
+        <p>The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.
+          It is often assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.</p>
+        <a href="https://www.youtube.com/watch?v=v6-4wh_o9iM&t=185s">Watch Video</a>
+      </div>
+      <div className="content-right">
+      <img src={image1} />
+      </div>
+    </div>
+  )
+}
+
+const cssCont = () => {
+  return (
+    <div id="content1" className="content">
+      <div className="content-left">
+        <h1>Learn CSS</h1>
+        <p>CSS stands for Cascading Style Sheets. It is a style sheet language which is used to describe the look and formatting of a document written in markup language.
+          t provides an additional feature to HTML. It is generally used with HTML to change the style of web pages and user interfaces.</p>
+        <a href="https://www.youtube.com/watch?v=v6-4wh_o9iM&t=185s">Watch Video</a>
+      </div>
+      <div className="content-right">
+      <img src={image2} />
+      </div>
+    </div>
+  )
+}
+
+const jsCont = () => {
+  return (
+    <div id="content1" className="content">
+      <div className="content-left">
+        <h1>Learn JAVASCRIPT</h1>
+        <p>JavaScript (js) is a light-weight object-oriented programming language which is used by several websites for scripting the webpages.
+          It is an interpreted, full-fledged programming language that enables dynamic interactivity on websites.</p>
+        <a href="https://www.youtube.com/watch?v=v6-4wh_o9iM&t=185s">Watch Video</a>
+      </div>
+      <div class="content-right">
+      <img src={image3} />
+      </div>
+    </div>
+  )
+}
+
+function App() {
+
+  const [cont, setCont] = useState(htmlCont);
+  const [countCss, setCountCss] = useState(0);
+  const [countHtml, setCountHtml] = useState(0);
+  const [countJs, setCountJs] = useState(0);
+
+
+  const handeleCont = (event) => {
+    console.log(event.target.name);
+
+
+    if (event.target.name === 'html'){
+      setCont(htmlCont);
+    }
+    else if(event.target.name === 'css'){
+
+      // count logic Simple explanation with useState
+      setCountCss((prev) =>{
+        return prev+1;
+      })
+
+      // container logic with useState
+      setCont(cssCont);
+    }
+    else if(event.target.name === 'js'){  
+      setCont(jsCont);
+    }
+    
+     
+  }
+
+  return (
+    <div classNameName="App">
+      <div className="hero">
+        
+        <div className="btn-box">
+          <button id="btn1" name = 'html' onClick={handeleCont}><i className="fa fa-html5"></i>HTML</button>
+          <button id="btn2" name = 'css' onClick={handeleCont}><i className="fa fa-css3"></i>CSS { countCss }</button>
+          <button id="btn3" name = 'js' onClick={handeleCont}><i className="fa fa-codepen"></i>JAVASCRIPT</button>
+        </div>
+
+       
+
+        {
+          cont  
+        }
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
